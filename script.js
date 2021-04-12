17b360757cb5a7421781361fc8d131bce9d847eb
acparkgh <acpark123@gmail.com>
2021-04-12 03:10:51 -0400

    review javaScript fundamentals

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 let js = 'amazing';
-if(js === 'amazing') alert("JavaScript is Fun!");
+// if(js === 'amazing') alert("JavaScript is Fun!");
 console.log(40 + 8 + 23 - 10);
-console.log("Hello World!!!")
+
+/*  multi-line comments
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+*/
+
+let years = 'JM';
+let $function = 27;
+let person = 'jonas';
+// console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof person);
+console.log(typeof 23);
+console.log(typeof "Hello World!");
+
+
+// this is how to write comments
+
