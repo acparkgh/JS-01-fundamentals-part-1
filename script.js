25d2e488894d9b09303ecab9b375dc00c856600d
acparkgh <acpark123@gmail.com>
2021-04-13 16:23:31 -0400

    javascript practice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 // if (js === 'amazing') alert("JavaScript is Fun!");
 /*  multi-line comments
@@ -13,35 +13,17 @@ console.log(firstName);
 
 */
 
-let js = 'amazing';
-let years = 'JM';
-let $function = 27;
-let person = 'jonas';
-// console.log(true);
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
-console.log(typeof javascriptIsFun);
-console.log(typeof person);
-console.log(typeof 23);
-console.log(typeof "Hello World!");
 
-const currentYear = 2021;
-const ageAndrew = currentYear - 1968;
-console.log("Andrew is", ageAndrew, 'years old');
 
-const firstName = 'Andrew';
-const lastName = 'Park';
 
-console.log(firstName + ' ' + lastName);
 
-let x = 10 + 5;
-console.log(x);
-x += 10;
-console.log(x);
-x++;
-console.log(x);
-x+= 10;
-console.log(x);
+
+
+
+
+
+
+
 
 
 
